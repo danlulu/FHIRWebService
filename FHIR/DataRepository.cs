40ec4ee066896f3a94f856e13a84e510efa4942a
Dan Lulu <dan.lulu@gmail.com>
2016-12-09 15:13:04 +1300

    Removed unused namespaces.

--- FHIR/DataRepository.cs
+++ FHIR/DataRepository.cs
@@ -1,11 +1,5 @@
 ﻿using FHIR.DTOs;
-using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.Configuration;
 
 namespace FHIR
 {
